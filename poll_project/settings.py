12343ba7b8007e291d1fea153c533aedea82fd86
Ваше Имя <you@example.com>
2021-01-20 13:15:37 +0300

    some text

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -134,7 +134,5 @@ STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
